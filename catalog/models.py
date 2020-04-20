5b12816ee2639f2db26cae5a6c518c17ffbebdfb
Latarax <talhakhan004@gmail.com>
2020-04-20 17:47:05 -0400

    fixed css, connected db, and pulled booklist from db

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    username = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    condition = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    quantity = models.IntegerField()
+    availability = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import psycopg2
 
 def home(request):
     return render(request, 'catalog/index.html') 
@@ -11,7 +12,15 @@ def mission(request):
     return render(request, 'catalog/mission.html')
 
 def booklist(request):
-    return render(request, 'catalog/booklist.html')
+    conn = psycopg2.connect( host="ec2-3-223-21-106.compute-1.amazonaws.com", user="fdnmfvethgnqkt", password="d8f82d08af92d50f7a117fb1537604bbbd2be24120847dce5910c0eed5eb52ad", dbname="d7hh193tgfderk" )
+    cur = conn.cursor()
+    sample_query = "select * from library"
+    cur.execute(sample_query)
+    records = cur.fetchall()
+
+    context = {'records':records}
+
+    return render(request, 'catalog/booklist.html', context)
 
 def checkout(request):
     return render(request, 'catalog/checkout.html')
